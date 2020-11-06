7e0567f36c93410717472f2da4eb5c90edee9016
jus37 <jus_37@outlook.jp>
2020-11-06 15:59:16 +0900

    itemのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,18 @@ class Item < ApplicationRecord
   belongs_to :cost
   belongs_to :day
   belongs_to :status
+
+  with_options presence: true do
+    validates :name
+    validates :content
+    validates :price
+    validates :image
+  end
+  with_options numbericality: {other_than: 0} do
+    validates :category_id
+    validates :status_id
+    validates :cost_id
+    validates :area_id
+    validates :days
+  end
 end
