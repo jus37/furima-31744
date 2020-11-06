f8d8c1b32340b5497ef7f3c3f056b0dcb0d9963c
jus37 <jus_37@outlook.jp>
2020-11-06 12:25:49 +0900

    バリデーションの記載方法修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
+  with_options presence: true,
+    validates :nickname, presence: true
+    validates :birthday, presence: true
+  end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, presence: true, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }
 
@@ -16,5 +18,4 @@ class User < ApplicationRecord
     validates :first_name_kana
     validates :last_name_kana
   end
-  validates :birthday, presence: true
 end
