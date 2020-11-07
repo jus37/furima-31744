94dc570d39829e2e4f00ac0c0308b0840cb60a0d
jus37 <jus_37@outlook.jp>
2020-11-07 16:53:21 +0900

    情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,25 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user.id
+      redirect_to root_path
+    end
+
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+
+  end
+
+
   private
 
   def item_params
--- app/javascript/price.js
+++ app/javascript/price.js
@@ -2,11 +2,15 @@ function price() {
   const form = document.getElementById("item-price")
   const tax = document.getElementById("add-tax-price");
   const profit = document.getElementById("profit")
-  form.addEventListener("keyup",() =>{
+  function caliculate(){
     const tax_price = parseInt(form.value /10)
     tax.innerHTML = `${tax_price}`
     const profit_price = form.value - tax_price
     profit.innerHTML = `${profit_price}`
+  }
+  caliculate()
+  form.addEventListener("keyup",() =>{
+    caliculate()
   })
 
 }
