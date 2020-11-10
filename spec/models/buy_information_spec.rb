8c6240eed3ef62560559fbbd29a3e45567231360
jus37 <jus_37@outlook.jp>
2020-11-10 21:22:38 +0900

    レビュー修正r3

--- spec/models/buy_information_spec.rb
+++ spec/models/buy_information_spec.rb
@@ -17,38 +17,44 @@ RSpec.describe BuyInformation, type: :model do
       it '郵便番号が空白である' do
         @order.postnumber = ''
         @order.valid?
-        expect(@order.errors.full_messages).to include
+        expect(@order.errors.full_messages).to include("Postnumber can't be blank", "Postnumber is invalid")
       end
       it '郵便番号が正しい方法で入力されていない' do
         @order.postnumber = '1111111'
         @order.valid?
-        expect(@order.errors.full_messages).to include
+        expect(@order.errors.full_messages).to include("Postnumber is invalid")
       end
       it '都道府県が---で入力されている' do
         @order.area_id = '0'
         @order.valid?
-        expect(@order.errors.full_messages).to include
+        expect(@order.errors.full_messages).to include("Area must be other than 0")
       end
       it '市区町村が空白である' do
         @order.area_town = ''
         @order.valid?
-        expect(@order.errors.full_messages).to include
+        expect(@order.errors.full_messages).to include("Area town can't be blank")
       end
       it '番地が空白である' do
         @order.area_street = ''
         @order.valid?
-        expect(@order.errors.full_messages).to include
+        expect(@order.errors.full_messages).to include("Area street can't be blank")
       end
       it '電話番号が空白である' do
         @order.telephonenumber = ''
         @order.valid?
-        expect(@order.errors.full_messages).to include
+        expect(@order.errors.full_messages).to include("Telephonenumber can't be blank", "Telephonenumber is invalid")
       end
       it '電話番号が12桁以上である' do
         @order.telephonenumber = '123456789012'
         @order.valid?
-        expect(@order.errors.full_messages).to include
+        expect(@order.errors.full_messages).to include("Telephonenumber is too long (maximum is 11 characters)")
       end
+      it 'トークンが空である' do
+        @order.token = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Token can't be blank")
+      end
+
     end
   end
 end
